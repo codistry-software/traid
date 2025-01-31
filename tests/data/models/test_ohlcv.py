5dc33e97ae5e94baf959bf1dc99c8f00675b6bbc
jumpiix <david@unterguggenberger.ch>
2025-01-31 09:16:29 +0100

    test: Add Kraken data parsing test

--- tests/data/models/test_ohlcv.py
+++ tests/data/models/test_ohlcv.py
@@ -21,3 +21,17 @@ def test_ohlcv_initialization():
     assert ohlcv.close == 48250.4
     assert ohlcv.volume == 10.5
 
+
+def test_from_kraken_data():
+    """Test if OHLCV correctly parses Kraken data format."""
+    raw_data = [1707566400, "48200.1", "48300.2", "48100.3", "48250.4", "48225.5", "10.5", 100]
+
+    ohlcv = OHLCV.from_kraken_data(raw_data)
+
+    assert isinstance(ohlcv, OHLCV)
+    assert ohlcv.timestamp == 1707566400
+    assert ohlcv.open == 48200.1
+    assert ohlcv.high == 48300.2
+    assert ohlcv.low == 48100.3
+    assert ohlcv.close == 48250.4
+    assert ohlcv.volume == 10.5
